a9f03d9f3b9d1a5b058f68c9b1394bedc3bf5da1
William Wong <wongww@usc.edu>
2020-07-29 18:29:26 -0700

    Update

--- server.js
+++ server.js
@@ -61,28 +61,7 @@ var app = express();
 
 
 //Still really confused about this line 
-app.use(express.static(path.join(__dirname, 'client/build'))).use(cors()).use(cookieParser());
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    console.log("MIDDELWARE USED");
-    // Pass to next layer of middleware
-    next();
-});
-// app.use(express.static(__dirname + '/public'))
-//    .use(cors())
-//    .use(cookieParser());
+app.use(cors()).use(cookieParser());
 
 app.get('/login', function(req, res) {
 
